c23ebc8407469ebd3ef62fdeacec75df221f28cd
unknown <soumya_aithal@hotmail.com>
2015-04-26 08:18:35 -0700

    This file defines functions that return  cached inverse of a matrix to avoid recomputation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This file defines functions that return  cached inverse of a matrix to avoid recomputation
 
-## Write a short comment describing this function
+## makeCacheMatrix is  a special matrix that caches the inverse of a invertible square matrix x,
+## It returns a list of accessor functions to retrieve the matrix and its inverse. The matrix input must be a square invertible matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Finds the inverse of the special matrix defined using makeCacheMatrix,
+## Stores the computed inverse in makeCacheMatrix for to avoid recomputation
+## Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
