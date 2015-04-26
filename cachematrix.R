be9a81b6c21aa0d457a4c5769c37fda516654651
soumyaVPS <soumya_aithal@hotmail.com>
2015-04-26 08:26:21 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,20 @@
 ##This file defines functions that return  cached inverse of a matrix to avoid recomputation
+##Sample usage: 
+##a = matrix(c(1,1,1,1), 2,2)
+##x1 = makeCacheMatrix(a)
+##cacheSolve(x1)
+##   [,1] [,2]
+##[1,]   -1  1.5
+##[2,]    1 -1.0
+##cacheSolve(x1)
+##getting cached data
+##     [,1] [,2]
+##[1,]   -1  1.5
+##[2,]    1 -1.0
 
 ## makeCacheMatrix is  a special matrix that caches the inverse of a invertible square matrix x,
-## It returns a list of accessor functions to retrieve the matrix and its inverse. The matrix input must be a square invertible matrix
+## It returns a list of accessor functions to retrieve the matrix and its inverse. 
+##The matrix input must be a square invertible matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -18,7 +31,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## Finds the inverse of the special matrix defined using makeCacheMatrix,
-## Stores the computed inverse in makeCacheMatrix for to avoid recomputation
+## Stores the computed inverse back in makeCacheMatrix for to avoid recomputation the next time
 ## Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
     inv <- x$getinverse()
